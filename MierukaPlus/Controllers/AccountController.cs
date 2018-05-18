35e4a323a2e03f57a4c74e411cdd95749a91f09e
Murat Demirci <mdemirci@outlook.com>
2018-05-18 18:11:00 +0300

    first version completed

--- MierukaPlus/Controllers/AccountController.cs
+++ MierukaPlus/Controllers/AccountController.cs
@@ -136,7 +136,7 @@ namespace MierukaPlus.Controllers
 
         //
         // GET: /Account/Register
-        [AllowAnonymous]
+        [Authorize]
         public ActionResult Register()
         {
             return View();
@@ -145,7 +145,7 @@ namespace MierukaPlus.Controllers
         //
         // POST: /Account/Register
         [HttpPost]
-        [AllowAnonymous]
+        [Authorize]
         [ValidateAntiForgeryToken]
         public async Task<ActionResult> Register(RegisterViewModel model)
         {
